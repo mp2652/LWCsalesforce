d1fe290bf01a8ebaae92a3f2b63369c2c996ed78
mp2652 <moriane.p@capretraite.fr>
2024-05-14 22:09:34 +0300

    add icon + error message

--- force-app/main/default/lwc/step2Form/step2Form.js
+++ force-app/main/default/lwc/step2Form/step2Form.js
@@ -3,7 +3,7 @@ import { LightningElement, api } from 'lwc';
 export default class Step2Form extends LightningElement {
     @api country;
     @api phoneNumber;
-    phoneError = false;
+    @api phoneError = false;
 
     countryOptions = [
         { label: 'USA', value: 'USA' },
